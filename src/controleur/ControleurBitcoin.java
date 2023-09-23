11d9206e7496cb9f80a3614c9eaf1367a18fadc5
SuperPitpit <petermiousse1@hotmail.com>
2023-09-22 20:54:06 -0400

    Sujet bitcoin fini

--- src/controleur/ControleurBitcoin.java
+++ src/controleur/ControleurBitcoin.java
@@ -1,8 +1,12 @@
 package controleur;
 
+import java.util.List;
+
 import com.sun.media.jfxmedia.logging.Logger;
 
 import architecture.Controleur;
+import donnee.BitcoinDAO;
+import modele.Bitcoin;
 import vue.VueBitcoin;
 
 public class ControleurBitcoin extends Controleur {
@@ -15,12 +19,24 @@ public class ControleurBitcoin extends Controleur {
 	
 	public void initialiser()
 	{
+		BitcoinDAO bitcoinDAO = new BitcoinDAO();
+		List<Bitcoin> monnaies = bitcoinDAO.listerMonnaie();
+		Bitcoin monnaie = monnaies.get(0);
+		// System.out.println(monnaie.getCode());
 		
 		VueBitcoin vue = VueBitcoin.getInstance();
+		
+		vue.afficherCode(monnaie.getCode());
+		vue.afficherSymbole(monnaie.getSymbole());
+		vue.afficherTaux(monnaie.getTaux());
+		vue.afficherDescription(monnaie.getDescription());
+		
+		/*
 		vue.afficherCode("CAN");
 		vue.afficherSymbole("$");
 		vue.afficherTaux((float)32323.33);
 		vue.afficherDescription("C'est le dollard canadien");
+		*/
 	}
 
 }
